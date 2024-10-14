bec8899437c1c3d45dce2fc8ffc9123356185e69
Renato <andresuyaguari200567@gmail.com>
2024-10-14 00:36:44 -0700

    FINALIZACION DE PROGRAMA

--- src/App.java
+++ src/App.java
@@ -1,14 +1,15 @@
-import java.util.Arrays; 
+import java.util.Arrays;
 import java.util.Scanner;
-
-public class App {
-    public static void main(String[] args) throws Exception {
+public class App 
+{
+    public static void main(String[] args) throws Exception 
+    {
         Metodos ordenamientoOpcion = new Metodos();
         Scanner leer = new Scanner(System.in);
         int opcion = 0;
         int[] arreglo = null;
-
-        while (true) {
+        while (true) 
+        {
             System.out.println("Bienvenido al programa de metodos");
             System.out.println("Menu Principal");
             System.out.println("1. Ingrese un arreglo");
@@ -16,26 +17,25 @@ public class App {
             System.out.println("0. Salir");
             System.out.print("Seleccione una opción: ");
             opcion = leer.nextInt();
-
-            switch (opcion) {
+            switch (opcion) 
+            {
                 case 1:
                     System.out.print("Ingrese el tamaño de su arreglo: ");
                     int tamanio = leer.nextInt();
                     arreglo = new int[tamanio];
-                    for (int i = 0; i < tamanio; i++) {
+                    for (int i = 0; i < tamanio; i++) 
+                    {
                         System.out.print("Ingrese el valor para el elemento " + (i + 1) + ": ");
                         arreglo[i] = leer.nextInt();
                     }
-                    // Aquí imprimimos el arreglo usando Arrays.toString()
                     System.out.println("Arreglo ingresado correctamente: " + Arrays.toString(arreglo));
                     break;
-
                 case 2:
-                    if (arreglo == null) {
+                    if (arreglo == null) 
+                    {
                         System.out.println("Primero debe ingresar un arreglo.");
                         break;
                     }
-
                     System.out.println("Escoja un método de ordenamiento:");
                     System.out.println("1. Método Burbuja");
                     System.out.println("2. Método Selección");
@@ -43,47 +43,41 @@ public class App {
                     System.out.println("4. Método Burbuja Mejorado");
                     System.out.print("Seleccione una opción: ");
                     int metodoOpcion = leer.nextInt();
-
                     System.out.println("Seleccione el orden:");
                     System.out.println("1. Ascendente");
                     System.out.println("2. Descendente");
                     int ordenOpcion = leer.nextInt();
                     boolean ascendente = (ordenOpcion == 1);
-
                     System.out.println("¿Quiere ver los pasos intermedios?");
                     System.out.println("1. Sí");
                     System.out.println("2. No");
                     int verPasosOpcion = leer.nextInt();
                     boolean mostrarPasos = (verPasosOpcion == 1);
-
                     // Llamar al método de ordenamiento correspondiente
-                    switch (metodoOpcion) {
+                    switch (metodoOpcion) 
+                    {
                         case 1: // Método Burbuja
-                            arreglo = ordenamientoOpcion.sortByBubble(arreglo);
+                            arreglo = ordenamientoOpcion.sortByBubble(arreglo, mostrarPasos);
                             break;
                         case 2: // Método Selección
-                            arreglo = ordenamientoOpcion.selectionSort(arreglo, ascendente);
+                            arreglo = ordenamientoOpcion.selectionSort(arreglo, ascendente, mostrarPasos);
                             break;
                         case 3: // Método Inserción
-                            arreglo = ordenamientoOpcion.insertionSort(arreglo);
+                            arreglo = ordenamientoOpcion.insertionSort(arreglo, mostrarPasos);
                             break;
-                        case 4: // Método Burbuja Mejorado 
-                        arreglo = ordenamientoOpcion.sortByBubbleAjuste(arreglo);
+                        case 4: // Método Burbuja Mejorado
+                            arreglo = ordenamientoOpcion.sortByBubbleAjuste(arreglo, mostrarPasos);
                             break;
                         default:
                             System.out.println("Opción no válida.");
                     }
-
-                    // Imprimir el arreglo ordenado
                     System.out.println("Arreglo ordenado:");
                     ordenamientoOpcion.printArreglo(arreglo);
                     break;
-
                 case 0:
                     System.out.println("Gracias por usar el programa. ¡Hasta luego!");
                     leer.close();
                     return;
-
                 default:
                     System.out.println("Opción no válida. Por favor, intente de nuevo.");
             }
--- src/Metodos.java
+++ src/Metodos.java
@@ -1,27 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class Metodos {
-    public Metodos() {}
-
-    private String arrayToString(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i]);
-            if (i < arr.length - 1) sb.append(", ");
-        }
-        return String.format("%-30s", sb.toString());
+    public Metodos() { 
     }
-
-    // Método de ordenamiento por Burbuja
-    public int[] sortByBubble(int[] arreglo) {
+    private String arrayToString(int[] arregloSturctura) 
+    {
+        StringBuilder structura = new StringBuilder();
+        for (int i = 0; i < arregloSturctura.length; i++) 
+        {
+            structura.append(arregloSturctura[i]);
+            if (i < arregloSturctura.length - 1) structura.append(", ");
+        }
+        return String.format("%-30s", structura.toString());
+    }
+    // Método de ordenamiento Burbuja
+    public int[] sortByBubble(int[] arreglo, boolean mostrarPasos) 
+    {
         int tamanio = arreglo.length;
         List<String> pasos = new ArrayList<>();
-        
-        for (int i = 0; i < tamanio - 1; i++) {
-            for (int j = 0; j < tamanio - 1 - i; j++) {
-                pasos.add(String.format("%s valor a: %-2d valor b: %d", arrayToString(arreglo), arreglo[j], arreglo[j + 1]));
-                if (arreglo[j] > arreglo[j + 1]) {
+        for (int i = 0; i < tamanio - 1; i++) 
+        {
+            for (int j = 0; j < tamanio - 1 - i; j++) 
+            {
+                String paso = String.format("%s valor a: %-2d valor b: %d", 
+                               arrayToString(arreglo), arreglo[j], arreglo[j + 1]);
+                pasos.add(paso);
+                if (arreglo[j] > arreglo[j + 1]) 
+                {
                     int aux = arreglo[j];
                     arreglo[j] = arreglo[j + 1];
                     arreglo[j + 1] = aux;
@@ -29,54 +34,63 @@ public class Metodos {
                 }
             }
         }
-        
-        System.out.println("Método Burbuja:");
-        for (String paso : pasos) {
-            System.out.println(paso);
+        if (mostrarPasos) 
+        {
+            System.out.println("Método Burbuja:");
+            for (String paso : pasos) {
+                System.out.println(paso);
+            }
         }
         return arreglo;
     }
-
-    // Método de ordenamiento por Selección
-    public int[] selectionSort(int[] arreglo, boolean ascendente) {
+    // Método de ordenamiento Selección
+    public int[] selectionSort(int[] arreglo, boolean ascendente, boolean mostrarPasos) 
+    {
         int tamanio = arreglo.length;
         List<String> pasos = new ArrayList<>();
-        
-        for (int i = 0; i < tamanio - 1; i++) {
+        for (int i = 0; i < tamanio - 1; i++) 
+        {
             int indice = i;
-            for (int j = i + 1; j < tamanio; j++) {
-                pasos.add(String.format("%s pos menor=%d pos j=%d", arrayToString(arreglo), indice, j));
-                if (ascendente ? arreglo[j] < arreglo[indice] : arreglo[j] > arreglo[indice]) {
+            for (int j = i + 1; j < tamanio; j++) 
+            {
+                String paso = String.format("%s pos menor=%d pos j=%d", 
+                               arrayToString(arreglo), indice, j);
+                pasos.add(paso);
+                if (ascendente ? arreglo[j] < arreglo[indice] : arreglo[j] > arreglo[indice]) 
+                {
                     indice = j;
                 }
             }
-            if (indice != i) {
+            if (indice != i) 
+            {
                 int aux = arreglo[indice];
                 arreglo[indice] = arreglo[i];
                 arreglo[i] = aux;
                 pasos.add(String.format("%s (intercambio)", arrayToString(arreglo)));
             }
         }
-        
-        System.out.println("Método Selección:");
-        for (String paso : pasos) {
-            System.out.println(paso);
+        if (mostrarPasos) 
+        {
+            System.out.println("Método Selección:");
+            for (String paso : pasos) 
+            {
+                System.out.println(paso);
+            }
         }
         return arreglo;
     }
-
-    // Método de ordenamiento por Inserción
-    public int[] insertionSort(int[] arreglo) {
+    // Método de ordenamiento Inserción
+    public int[] insertionSort(int[] arreglo, boolean mostrarPasos) 
+    {
         int n = arreglo.length;
         List<String> pasos = new ArrayList<>();
-        
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) 
+        {
             int valorActual = arreglo[i];
             int j = i - 1;
-            
             pasos.add(String.format("%s valor actual: %d", arrayToString(arreglo), valorActual));
-            
-            while (j >= 0 && arreglo[j] > valorActual) {
+            while (j >= 0 && arreglo[j] > valorActual) 
+            {
                 pasos.add(String.format("%s comparando con: %d", arrayToString(arreglo), arreglo[j]));
                 arreglo[j + 1] = arreglo[j];
                 j--;
@@ -85,25 +99,29 @@ public class Metodos {
             arreglo[j + 1] = valorActual;
             pasos.add(String.format("%s (inserción)", arrayToString(arreglo)));
         }
-        
-        System.out.println("Método Inserción:");
-        for (String paso : pasos) {
-            System.out.println(paso);
+        if (mostrarPasos) {
+            System.out.println("Método Inserción:");
+            for (String paso : pasos) 
+            {
+                System.out.println(paso);
+            }
         }
         return arreglo;
     }
-
-    // Método Burbuja con Ajuste
-    public int[] sortByBubbleAjuste(int[] arreglo) {
+    // Método Burbuja Con Ajuste
+    public int[] sortByBubbleAjuste(int[] arreglo, boolean mostrarPasos) 
+    {
         int tamanio = arreglo.length;
         boolean swapped;
         List<String> pasos = new ArrayList<>();
-        
-        for (int i = 0; i < tamanio - 1; i++) {
+        for (int i = 0; i < tamanio - 1; i++) 
+        {
             swapped = false;
-            for (int j = 0; j < tamanio - 1 - i; j++) {
+            for (int j = 0; j < tamanio - 1 - i; j++) 
+            {
                 pasos.add(String.format("%s valor a: %-2d valor b: %d", arrayToString(arreglo), arreglo[j], arreglo[j + 1]));
-                if (arreglo[j] > arreglo[j + 1]) {
+                if (arreglo[j] > arreglo[j + 1]) 
+                {
                     int aux = arreglo[j];
                     arreglo[j] = arreglo[j + 1];
                     arreglo[j + 1] = aux;
@@ -111,21 +129,24 @@ public class Metodos {
                     pasos.add(String.format("%s (intercambio)", arrayToString(arreglo)));
                 }
             }
-            if (!swapped) {
+            if (!swapped) 
+            {
                 pasos.add("Arreglo ordenado antes de tiempo");
                 break;
             }
         }
-        
-        System.out.println("Método Burbuja con Ajuste:");
-        for (String paso : pasos) {
-            System.out.println(paso);
+        if (mostrarPasos) 
+        {
+            System.out.println("Método Burbuja con Ajuste:");
+            for (String paso : pasos) 
+            {
+                System.out.println(paso);
+            }
         }
         return arreglo;
     }
-
-    // Método para imprimir el arreglo
-    public void printArreglo(int[] arreglo) {
+    public void printArreglo(int[] arreglo) 
+    {
         System.out.println(arrayToString(arreglo));
     }
 }
